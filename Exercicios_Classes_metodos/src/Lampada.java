14aae4a34610fbeaa1cf2836109482b936191235
Yago Menezes <tenn.menezes@gmail.com>
2026-02-03 11:43:23 -0300

    Feature: Adicionando exercícios finalizados de classes, atributos e métodos void, com parâmetros e com retorno.

---
+++ Exercicios_Classes_metodos/src/Lampada.java
@@ -0,0 +1,23 @@
+class Lampada {
+
+    String modo;
+
+    void ligarLampada() {
+
+        modo = "Ligada";
+
+    }
+
+    void desligarLampada() {
+
+        modo = "Desligada";
+
+    }
+
+    void estadoAtualDaLampada() {
+
+        System.out.println("A lâmpada atualmente está: " + modo);
+
+    }
+
+}
